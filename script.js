5e3de557673d06ce49cb8a782fffb72fddc10000
st4r.rraa_ <2400018105@webmail.uad.ac.id>
2025-05-13 17:45:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const name = "Mutiara";
+    console.log("Selamat datang di website " + name + "!");
+    alert("Haloo! Selamat Datang di Profil Mutiara!");
+});
